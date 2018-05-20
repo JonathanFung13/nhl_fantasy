0904b599e00a1cffb45700892206e474e74bc2e8
Jonathan Fung <jfung9@gatech.edu>
2018-05-20 00:29:14 -0700

    starting to push to google sheet

--- leaders.py
+++ leaders.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
 import requests
 #import lxml.html
 from pprint import pprint
@@ -12,16 +16,16 @@ import pandas as pd
 def get_string_season(season):
     return str(season - 1) + str(season)
 
+def request_json(url):
+    resp_json = requests.get(url).text
+    return json.loads(resp_json)['data']
 
 def get_skater_stats(season):
-    stringSeason = get_string_season(season)
     req = 'http://www.nhl.com/stats/rest/skaters?isAggregate=false&reportType=basic&isGame=false' + \
             '&reportName=skatersummary&sort=[{"property":"points","direction":"DESC"}]&cayenneExp=gameTypeId=2' + \
-            'and seasonId%3E=' + stringSeason + 'and seasonId%3C=' + stringSeason
-    resp_json = requests.get(req).text
-    leaders = json.loads(resp_json)['data']
+            'and seasonId%3E=' + season + 'and seasonId%3C=' + season
+    skaters = pd.DataFrame(data=request_json(req))
 
-    skaters = pd.DataFrame(data=leaders)
     skaters = skaters[['playerId', 'playerPositionCode', 'playerName', 'points']]
     maskForwards = skaters['playerPositionCode'] != 'D'
     skaters.loc[maskForwards, 'playerPositionCode'] = 'F'
@@ -29,14 +33,11 @@ def get_skater_stats(season):
     return skaters
 
 def get_goalie_stats(season):
-    stringSeason = get_string_season(season)
     req = 'http://www.nhl.com/stats/rest/goalies?isAggregate=false&reportType=goalie_basic&isGame=false' +\
           '&reportName=goaliesummary&sort=[{"property":"saves","direction":"DESC"}]&cayenneExp=gameTypeId=2' +\
-          ' and seasonId%3E=' + stringSeason + 'and seasonId%3C=' + stringSeason
-    resp_json = requests.get(req).text
-    leaders = json.loads(resp_json)['data']
+          ' and seasonId%3E=' + season + 'and seasonId%3C=' + season
+    goalies = pd.DataFrame(data=request_json(req))
 
-    goalies = pd.DataFrame(data=leaders)
     goalies['points'] = goalies['saves'] / 9.0 - goalies['goalsAgainst']
     goalies['points'] = goalies['points'].round()
     maskNegatives = goalies['points'] < 0
@@ -47,8 +48,46 @@ def get_goalie_stats(season):
     goalies = goalies[['playerId', 'playerPositionCode', 'playerName', 'points']]
     return goalies
 
-skaters = get_skater_stats(2018)
-goalies = get_goalie_stats(2018)
+if __name__ == '__main__':
+    stringSeason = get_string_season(2018)
+    skaters = get_skater_stats(stringSeason)
+    goalies = get_goalie_stats(stringSeason)
 
-all_stats = pd.concat([skaters,goalies], axis=0)
-print(all_stats.sort_values('playerName'))
+    all_stats = pd.concat([skaters, goalies], axis=0)
+    all_stats = all_stats.sort_values('playerName')
+
+    # Setup the Sheets API
+    SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+    store = file.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    SPREADSHEET_ID = '1l6_ivIATcLNeZyPn0297MVr2ZiJlqIW2P8NXSAWF_nc'
+    RANGE_NAME = 'nhl_leaders!A1'
+    #result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+    #                                             range=RANGE_NAME).execute()
+    # How the input data should be interpreted.
+    value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+
+    for i, j in enumerate(all_stats):
+        print(i, j)
+
+
+    value_range_body = {
+        # TODO: Add desired entries to the request body. All existing entries
+        # will be replaced.
+        'values': [[row for row in all_stats.itertuples()]]
+    }
+    print(value_range_body)
+
+    request = service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+                                                     valueInputOption=value_input_option,
+                                                     body=value_range_body)
+    response = request.execute()
+
+    # TODO: Change code below to process the `response` dict:
+    pprint(response)
