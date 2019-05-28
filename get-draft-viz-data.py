a15fb5568b976d3d65fd738b0f8940c0edbd3207
Jonathan Fung <jfung9@gatech.edu>
2019-05-28 16:25:48 -0700

    Fixing issue where some players have no position appearing.  Needed to refactor draft data request to accomplish this.

---
+++ get-draft-viz-data.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import get_NHL_stats as gs
+import pandas as pd
+
+def get_career_stats(start, end, regularSeason):
+
+    # Get skater and goalie stats, combine them in a dataframe.
+    skaters = gs.get_skater_stats(start, end, regularSeason)
+    skaters = skaters[["playerName", "playerPositionCode", "playerNationality", "playerHeight", "playerWeight",
+                       "playerId", "gamesPlayed", "goals", "assists", "points", "pointsPerGame",
+                       "playerBirthCity", "playerBirthCountry", "playerBirthDate"]]
+
+    goalies = gs.get_goalie_stats(start, end, regularSeason)
+    goalies = goalies[["playerName", "playerPositionCode", "playerNationality", "playerHeight", "playerWeight",
+                       "playerId", "gamesPlayed", "shotsAgainst", "saves", "shutouts", "savePctg",
+                       "playerBirthCity", "playerBirthCountry", "playerBirthDate"]]
+
+    all_stats = pd.concat([skaters, goalies], axis=0, sort=False)
+    all_stats = all_stats.sort_values("playerName")
+    all_stats.fillna(0, inplace=True)
+
+    # all_stats = all_stats.rename(columns={"playerName": "PlayerName", "playerPositionCode": "Pos",
+    #                                   "playerNationality": "Nationality", "playerHeight": "Ht", "playerWeight": "Wt",
+    #                                   "playerId": "PlayerID", "gamesPlayed": "GamesPlayed", "goals": "Goals",
+    #                                   "assists": "Assists", "points": "Points", "pointsPerGame": "PPG",
+    #                                   "playerBirthCity": "BirthCity", "playerBirthCountry": "BirthCountry",
+    #                                   "playerBirthDate": "BirthDate"})
+
+    return all_stats
+
+def update_team_names(df):
+    # Update names of relocated franchises.
+    df.loc[df["team.name"]=="Phoenix Coyotes", "team.name"] = "Arizona Coyotes"
+    df.loc[df["team.name"]=="Atlanta Thrashers", "team.name"] = "Winnipeg Jets"
+
+    return df
+
+def set_statuses(df):
+    df.loc[df["team.name"] != df["teamName"], "status"] = "other_team"
+    df.loc[df["team.name"] == df["teamName"], "status"] = "active"
+    df.loc[df["teamName"].isnull(), "status"] = "inactive"
+
+    max_year = df["year"].max()
+    df["gpPerYear"] = df["gamesPlayed"] / (max_year - df["year"] + 1)
+    df["gpClass"] = 1
+    df.loc[df["gpPerYear"]>=20, "gpClass"] = 2
+    df.loc[df["gpPerYear"]>=40, "gpClass"] = 3
+    df.loc[df["gpPerYear"]>=60, "gpClass"] = 4
+
+    df["ppgClass"] = 1
+    df.loc[df["pointsPerGame"]>=0.25, "ppgClass"] = 2
+    df.loc[df["pointsPerGame"]>=0.5, "ppgClass"] = 3
+    df.loc[df["pointsPerGame"]>=0.75, "ppgClass"] = 4
+    df.loc[df["pointsPerGame"]>=1.0, "ppgClass"] = 5
+
+    return df
+
+def clean_data(df):
+    df.loc[df["birthCity"].isnull(), "birthCity"] = df["playerBirthCity"]
+    df.loc[df["birthCountry"].isnull(), "birthCountry"] = df["playerBirthCountry"]
+    df.loc[df["birthDate"].isnull(), "birthDate"] = df["playerBirthDate"]
+    df.loc[df["nationality"].isnull(), "nationality"] = df["playerNationality"]
+    df.loc[df["playerId"].isnull(), "playerId"] = df["nhlPlayerId"]
+    df.loc[df["position"].isnull(), "position"] = df["primaryPosition.abbreviation"]
+    df.loc[df["position"].isnull(), "position"] = "U"
+
+    return df
+
+def reduce_columns(df):
+    # columns = ['amateurLeague.link', 'amateurLeague.name', 'amateurTeam.link', 'amateurTeam.name', 'birthCity', 'birthCountry', 'birthDate', 'birthStateProvince', 'draftStatus', 'firstName', 'fullName', 'height', 'id', 'lastName', 'link', 'nationality', 'nhlPlayerId', 'pickInRound', 'pickOverall', 'primaryPosition.abbreviation', 'primaryPosition.code', 'primaryPosition.name', 'primaryPosition.type', 'prospect.fullName', 'prospect.id', 'prospect.link', 'prospectCategory.id', 'prospectCategory.name', 'prospectCategory.shortName', 'round', 'shootsCatches', 'team.id', 'team.link', 'team.name', 'weight', 'year', 'name_lower', 'playerName', 'playerPositionCode', 'playerNationality', 'playerHeight', 'playerWeight', 'playerId', 'gamesPlayed', 'goals', 'assists', 'points', 'pointsPerGame', 'playerBirthCity', 'playerBirthCountry', 'playerBirthDate', 'shotsAgainst', 'saves', 'shutouts', 'savePctg', 'fullName_y', 'jerseyNumber', 'position', 'teamName', 'Status', 'gpPerYear', 'gpClass', 'ppgClass']
+    columns = ['year', 'round', 'pickInRound', 'pickOverall', 'team.name', 'prospect.fullName',
+               'position', 'nationality', 'height', 'weight', 'playerHeight', 'playerWeight',
+               'birthCity', 'birthCountry', 'birthDate',
+               'amateurLeague.name', 'amateurTeam.name', 'playerId',
+               'gamesPlayed', 'goals', 'assists', 'points', 'pointsPerGame',
+               'shotsAgainst', 'saves', 'shutouts', 'savePctg', 'jerseyNumber', 'teamName',
+               'status', 'gpPerYear', 'gpClass', 'ppgClass']
+
+    df = df[columns]
+    return df
+
+def main():
+    start_season = 2008
+    end_season = 2019
+    regular_season = True
+
+    if False:
+        drafts = gs.get_drafts(start_season, end_season) # Takes a loonnnggg time to run
+        players = get_career_stats(start_season, end_season, regular_season)
+        rosters = gs.get_rosters()
+
+        gs.save_csv("drafts.csv", drafts)
+        gs.save_csv("players.csv", players)
+        gs.save_csv("rosters.csv", rosters)
+    else:
+        drafts = gs.load_csv("drafts.csv")
+        players = gs.load_csv("players.csv")
+        rosters = gs.load_csv("rosters.csv")
+
+    drafts = update_team_names(drafts)
+    drafts = drafts.sort_values(["team.name", "year", "round"], ascending=[1, 0, 1])
+
+    # Merge all data into one dataframe
+    drafts['name_lower'] = drafts['prospect.fullName'].str.lower()
+    players['name_lower'] = players['playerName'].str.lower()
+    rosters['name_lower'] = rosters['fullName'].str.lower()
+    draft_data = pd.merge(drafts, players, how="left", on="name_lower", sort=False, suffixes=("", "_x"))
+    draft_data = pd.merge(draft_data, rosters, how="left", on="name_lower", sort=False, suffixes=("", "_y"))
+
+    # Update positions and set statuses for each filter in the visuzalization.  Then get rid of unneeded columns.
+    draft_data = set_statuses(draft_data)
+    draft_data = clean_data(draft_data)
+    draft_data = reduce_columns(draft_data)
+
+    gs.save_csv("draft_data.csv", draft_data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ get_NHL_stats.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# from __future__ import print_function
+import requests
+import json
+import pandas as pd
+from pandas.io.json import json_normalize
+import os
+
+OUTPUT_FOLDER = "output"
+
+def get_string_season(season):
+    return str(season - 1) + str(season)
+
+def get_game_type(regular_season=True):
+    if regular_season: # regular season =2, playoffs = 3
+        gametype = 2
+    else:
+        gametype = 3
+
+    return gametype
+
+def request_json(url):
+    resp_json = requests.get(url).text
+    data = json.loads(resp_json)
+
+    if "success" in data: # Note there is no success key in response
+        raise Exception("Could not complete request for: " + url)
+    return data
+
+def get_skater_stats(start, end, regular_season=True):
+    start = get_string_season(start)
+    end = get_string_season(end)
+    type = get_game_type(regular_season)
+
+    req = "http://www.nhl.com/stats/rest/skaters?isAggregate=true&reportType=basic&isGame=false" + \
+          '&reportName=skatersummary&sort=[{"property":"points","direction":"DESC"}]&cayenneExp=gameTypeId=' + \
+          str(type) + "and seasonId%3E=" + start + "and seasonId%3C=" + end
+    skater_data = request_json(req)
+    skaters = pd.DataFrame(data=skater_data["data"])
+
+    return skaters
+
+def get_goalie_stats(start, end, regular_season=True):
+    start = get_string_season(start)
+    end = get_string_season(end)
+    type = get_game_type(regular_season)
+
+    req = "http://www.nhl.com/stats/rest/goalies?isAggregate=true&reportType=goalie_basic&isGame=false" +\
+          '&reportName=goaliesummary&sort=[{"property":"saves","direction":"DESC"}]&cayenneExp=gameTypeId=' + \
+          str(type) + "and seasonId%3E=" + start + "and seasonId%3C=" + end
+    goalie_data = request_json(req)
+    goalies = pd.DataFrame(data=goalie_data["data"])
+
+    return goalies
+
+def get_rosters():
+    req = "https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster"
+    roster_data = request_json(req)
+
+    rosters = []
+    for teams in roster_data["teams"]:
+        for players in teams["roster"]["roster"]:
+            if "jerseyNumber" in players:
+                number = players["jerseyNumber"]
+            else:
+                number = 0
+
+            rosters.append({
+                "fullName": players["person"]["fullName"],
+                "jerseyNumber": number,
+                "position": players["position"]["abbreviation"],
+                "teamName": teams["name"]
+            })
+
+    rosters = pd.DataFrame.from_records(rosters)
+    rosters = rosters.sort_values("fullName")
+
+    return rosters
+
+def get_prospect_info(link):
+    if link[-4:] == "null":
+        return {}
+
+    req = "https://statsapi.web.nhl.com" + link
+    prospect_data = request_json(req)
+
+    if 'prospects' in prospect_data and len(prospect_data['prospects']) > 0:
+        return prospect_data['prospects'][0]
+    else:
+        return {}
+
+def get_drafts(start, end):
+    req = "https://statsapi.web.nhl.com/api/v1/draft/"
+    players = []
+
+    for draft_year in range(start, end):
+        draft_data = request_json(req+str(draft_year))
+        draft = pd.DataFrame(data=draft_data["drafts"])
+
+        for rounds in draft["rounds"]:
+            for round in rounds:
+                for pick in round["picks"]:
+                    print(pick["prospect"]["fullName"], pick["prospect"]["link"])
+                    player_details = get_prospect_info(pick["prospect"]["link"])
+                    players.append({**pick, **player_details})
+
+    drafts = pd.DataFrame.from_records(json_normalize(players))
+
+    return drafts
+
+def save_csv(filename, df):
+    if not os.path.isdir(OUTPUT_FOLDER):
+        os.makedirs(OUTPUT_FOLDER)
+
+    df.to_csv(os.path.join(OUTPUT_FOLDER, filename), index=False)
+
+def load_csv(filename):
+
+    df_filename = os.path.join(OUTPUT_FOLDER, filename)
+    if not os.path.isfile(df_filename):
+        raise Exception(filename + " not found.")
+
+    df = pd.read_csv(df_filename)
+    return df
