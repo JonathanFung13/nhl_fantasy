b1a36978e00e3e5774fe2496cf42e77a8b83cb8f
Jonathan Fung <jfung9@gatech.edu>
2019-07-05 17:07:25 -0700

    Adding support for arguments in command line for updates to behave differently.

--- ullhp-update.py
+++ ullhp-update.py
@@ -7,6 +7,7 @@ from oauth2client import file, client, tools
 import pandas as pd
 import datetime as dt
 import get_NHL_stats as gs
+import argparse
 
 def pushUpdatetoSheet(stats, SPREADSHEET_ID, sheetName):
     # Setup the Sheets API
@@ -51,9 +52,13 @@ def pushUpdatetoSheet(stats, SPREADSHEET_ID, sheetName):
 
     return
 
-def update_rosters(googleSheetID, sheet_name='nhl_rosters'):
+def update_rosters(googleSheetID, sheet_name='nhl_rosters', savefile=False):
     rosters = gs.get_rosters()
-    pushUpdatetoSheet(rosters, googleSheetID, sheet_name)
+
+    if savefile:
+        rosters.to_csv(sheet_name + ".csv", index=False)
+    else:
+        pushUpdatetoSheet(rosters, googleSheetID, sheet_name)
     return
 
 def get_skater_stats(end, type=True):
@@ -77,7 +82,7 @@ def get_goalie_stats(end,type=True):
                        'playerBirthDate', 'playerHeight', 'playerWeight']]
     return goalies
 
-def update_stats(endYearOfSeason, regularSeason, SPREADSHEET_ID, sheet_name='nhl_leaders'):
+def update_stats(endYearOfSeason, regularSeason, googleSheetID, sheet_name='nhl_leaders', savefile=False):
 
     # Get skater and goalie stats, combine them in a dataframe.
     skaters = get_skater_stats(endYearOfSeason, regularSeason)
@@ -86,15 +91,36 @@ def update_stats(endYearOfSeason, regularSeason, SPREADSHEET_ID, sheet_name='nhl
     all_stats = all_stats.sort_values('playerName')
     all_stats.fillna(0, inplace=True)
 
-    pushUpdatetoSheet(all_stats, SPREADSHEET_ID, sheet_name)
+    if savefile:
+        all_stats.to_csv(sheet_name + '.csv', index=False)
+    else:
+        pushUpdatetoSheet(all_stats, googleSheetID, sheet_name)
+
+def main():
+    parser = argparse.ArgumentParser(description='NHL Fantasy Stats')
+    parser.add_argument('-o', '--output', help='Save to file with True, save to Google Sheet with False.',
+                        default=False, dest='output_type')
+    parser.add_argument('-r', '--roster', help='Filename or sheet name to save rosters to.', default='nhl_rosters',
+                        dest='roster_sheetName')
+    parser.add_argument('-s', '--stats', help='Filename or sheet name to save stats to.', default='nhl_leaders',
+                        dest='leader_sheetName')
+    parser.add_argument('-y', '--year', help='The ending year of the season to get.', type=int, default=2019,
+                        dest='endYearOfSeason')
+    parser.add_argument('-t', '--type', help='Save regular season stats with True, playoffs with False', default=True,
+                        type=bool, dest='regularSeason')
+    parser.add_argument('-g', '--gsheet_id', help='Google Sheet ID to push updates to', dest='googleSheetID')
+    parser.add_argument('-u', '--update_type', help='Update rosters (1), update stats (2) or both (3)', type=int,
+                        default=3, choices=[1,2,3], dest='update_type')
+    args = parser.parse_args()
+
+    if not args.output_type and args.googleSheetID is None:
+        raise Exception("A Google Sheet ID must be supplied unless you want to save to a file.")
+
+    if args.update_type != 2:
+        update_rosters(args.googleSheetID, args.roster_sheetName, args.output_type)
+    if args.update_type > 1:
+        update_stats(args.endYearOfSeason, args.regularSeason, args.googleSheetID, args.leader_sheetName, args.output_type)
 
 if __name__ == "__main__":
+    main()
 
-    endYearOfSeason = 2019
-    regularSeason = True
-    googleSheetID = '1CEz-fbuBqrCl2EzEQlUBtsfRSMGDgxKikKB1cNqubPQ'
-    roster_sheetName = 'nhl_rosters'
-    leader_sheetName = 'nhl_leaders'
-
-    #update_rosters(googleSheetID, roster_sheetName)
-    update_stats(endYearOfSeason, regularSeason, googleSheetID)
