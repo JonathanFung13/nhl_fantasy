59010ae5fdbe9c70b2680660d2275d9783d32a64
JonathanFung13 <jfung9@gatech.edu>
2025-03-21 17:58:05 -0700

    Alternate auth flow.

--- update_stats.py
+++ update_stats.py
@@ -4,12 +4,10 @@
 import argparse
 import logging
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
 import datetime as dt
 import pytz
 import utilities as util
-import pickle
 import os.path
 
 
@@ -17,33 +15,13 @@ def push_update_to_sheet(stats, gsheet_id, sheet_name):
     # Setup the Sheets API
     # from https://developers.google.com/sheets/api/quickstart/python
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    credentials_file = 'service_account.json'  # Replace with your actual service account file
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    TOKEN_FILE = 'token.pickle'
-    TOKEN_PATH = f'/tmp/{TOKEN_FILE}'
-    if os.path.exists(TOKEN_FILE) and not os.path.exists(TOKEN_PATH):
-        with open(TOKEN_FILE, 'rb') as tmp_token:
-            creds = pickle.load(tmp_token)
-            with open(TOKEN_PATH, 'wb') as token:
-                pickle.dump(creds, token)
 
-    if os.path.exists(TOKEN_PATH):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open(TOKEN_PATH, 'wb') as token:
-            pickle.dump(creds, token)
+    if os.path.exists(credentials_file):
+        creds = Credentials.from_service_account_file(credentials_file, scopes=SCOPES)
+    else:
+        raise FileNotFoundError("Service account JSON file not found. Please ensure it's in the correct location.")
 
     service = build('sheets', 'v4', credentials=creds)
 
